c534f902c002e1be632d54a17c2cd806b17d3552
Pannawadeeg <pannawadeeg63@nu.ac.th>
2022-08-25 11:02:09 +0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res ){
+  res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res) {
+  console.log('hello - ' + req.params.person)
+  res.send('sey hello with ' + req.params.person)
+})
+
+
+let port = 8081
+
+app.listen(port,function(){
+  console.log('Server running on ' + port)
+})
